6a11d516f1e0b0306a18b2e1a0f7827f8d553ba6
iwswordpress <iwswordpress@gmail.com>
2021-08-28 13:35:50 +0100

    03-3

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+	res.status(200).json({ success: true, msg: 'GET all bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+	res.status(200).json({ success: true, msg: `GET bootcamp ${req.params.id} ` });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+	res.status(201).json({ success: true, msg: 'CREATE new bootcamp' });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+	res.status(200).json({ success: true, msg: `UPDATE bootcamp ${req.params.id} ` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+	res.status(200).json({ success: true, msg: `DELETE bootcamp ${req.params.id} ` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
