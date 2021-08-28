b66c38ff3645fefb4febf66d57d4aca21ec953b4
iwswordpress <iwswordpress@gmail.com>
2021-08-28 14:37:44 +0100

    03-6 Middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,14 +3,14 @@ require('../colors');
 
 // @desc    Logs route to console
 
-const logger = (req, res, next) => {
+const logger = (req, res, nextFn) => {
 	req.hello = 'Hello World';
 	req.user = { id: 2, auth: true, token: 'FJJHH_8876HHH' };
 	const output = `LOGGER: ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`;
 	console.log(colors.verbose(output));
 	// console.log(colors.warn.inverse('Middleware ran...'));
 	// console.log(colors.error.underline('Middleware did NOT run...'));
-	next();
+	nextFn();
 };
 
 module.exports = logger;
