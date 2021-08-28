d47f66c70c766b1a9246cd8a9e6cf565f9eb80ec
iwswordpress <iwswordpress@gmail.com>
2021-08-28 11:57:13 +0100

    INIT PROJECT

--- server.js
+++ server.js
@@ -1,51 +1,15 @@
-const http = require('http');
-const colors = require('colors');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-	{ id: 1, text: '1 todo one' },
-	{ id: 2, text: '2 todo two' },
-	{ id: 3, text: '3 todo three' },
-];
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-	console.log(req.method);
-	const { method, url } = req;
-	let body = [];
+const app = express();
 
-	const response = {
-		success: false,
-		data: null,
-	};
+const PORT = process.env.PORT || 5000;
 
-	if (method === 'GET' && url === '/todos') {
-		status = 200;
-		response.success = true;
-		response.data = todos;
-		console.log(JSON.stringify(response).blue.inverse);
-	} else if (method === 'POST' && url === '/todos') {
-		req.on('data', (chunk) => {
-			console.log('chunk', chunk);
-			body.push(chunk);
-		}).on('end', () => {
-			body = Buffer.concat(body).toString();
-			console.log('end', body);
-			const { id, text } = JSON.parse(body);
-			todos.push({ id, text });
-			status = 201;
-			response.success = true;
-			response.data = todos;
-			console.log(JSON.stringify(response).blue.inverse);
-		});
-	}
-	res.writeHead(202, {
-		'Content-Type': 'application/json',
-		'X-Powered-By': 'Node.js',
-	});
-
-	// console.log(req.headers.authorization.green.inverse);
-
-	res.end(JSON.stringify(response));
+app.listen(PORT, () => {
+	console.log(
+		`Server running in ${process.env.NODE_ENVIRONMENT} mode on PORT ${PORT} \nMongoDB on ${process.env.MONGODB_URL}`,
+	);
 });
-
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Server runing on PORT ${PORT}...`));
